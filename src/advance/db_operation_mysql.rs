bee8d195a11889f2f3d490f576161da3151c7c2c
Arpit1551 <arpitsharma20005@gmail.com>
2026-08-27 00:44:36 +0530

    add the connection pool in sql

--- src/advance/db_operation_mysql.rs
+++ src/advance/db_operation_mysql.rs
@@ -1,31 +1,38 @@
-use actix_web::{ HttpResponse, HttpServer, App, get, web, Responder };
-use sqlx::postgres::PgPoolOptions;
+use actix_web::{ App, HttpServer, HttpResponse, Responder, web, post };
+use sqlx::{PgPool, postgres::PgPoolOptions};
 
-#[get("/")]
-async fn landing() -> impl Responder {
-    HttpResponse::Ok().body("Welcome to the landing page!")
+#[post("/get_users")]
+async fn get_users(pool: web::Data<PgPool>) -> impl Responder {
+    
+    let users = sqlx::query("Select * from users")
+    .fetch_all(pool.get_ref())
+    .await
+    .unwrap();
+
+        HttpResponse::Ok().body(format!("User data {:?}", users))
+
 }
 
 
 #[actix_web::main]
-async fn main() -> std::io::Result<()>{
+async fn main() -> std::io::Result<()> {
 
     dotenvy::dotenv().ok();
 
-    let database_url = std::env::var("DATABASE_URL").expect("Database url must be set");
-        let pool = PgPoolOptions::new()
-        .max_connections(10)
-        .connect(&database_url)
-        .await
-        .expect("Failed to connect to PostgreSQL");
+    let db_url = std::env::var("DATABASE_URL").expect("Database url must be set!");
+    let pool = PgPoolOptions::new()
+    .max_connections(10)
+    .connect(&db_url)
+    .await
+    .expect("Database connection failed!");
 
     println!("Database connected!");
 
-    HttpServer::new( move ||
+    HttpServer::new( move || {
         App::new()
-            .app_data(web::Data::new(pool.clone()))
-            .service(landing)  
-    )
+        .app_data(web::Data::new(pool.clone()))
+        .service(get_users)
+    })
     .bind(("127.0.0.1",8080))?
     .run()
     .await
